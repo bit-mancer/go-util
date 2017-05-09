849cf007e513ecd999d573a55a24cf1efaf6b863
Mike Post <mike@bitmancer.me>
2017-05-08 20:02:14 -0700

    Add hostname as a built-in field, checked when a new logger is created. Expose the built-in field key names. Update the Audit comment to remove reference to “warn” level (this should be transparent to the caller).

--- logging/log.go
+++ logging/log.go
@@ -9,9 +9,17 @@ import (
 	"github.com/Sirupsen/logrus"
 )
 
-const sourceKey = "_source"
-const domainKey = "_domain"
-const auditKey = "_audit"
+// SourceKey is the map key used for logging sources (see NewLog).
+const SourceKey = "_source"
+
+// DomainKey is the map key used for logging domains (see NewDomainLogger).
+const DomainKey = "_domain"
+
+// HostKey is the map key used for logging the machine hostname (via os.Hostname).
+const HostKey = "_host"
+
+// AuditKey is the map key used for audit-related entries (see Audit).
+const AuditKey = "_audit"
 
 // Fields is a set of strutured log information (i.e. a map-compatible type).
 // Example: log.WithFields(logging.Fields{"myKey": myVal, "myOtherKey": myOtherVal})
@@ -35,8 +43,8 @@ type Logger interface {
 type logger logrus.Entry
 
 // NewLog returns a new Logger instance.
-// 	* 'source' is the optional source of the events (the root process that all logs under this logger and its domains
-//    will be tagged with).
+// 	* 'source' is the optional source of the events (the root that all logs under this logger and its domains will
+//    be tagged with).
 func NewLog(source string) Logger {
 	baseFormatter := &logrus.TextFormatter{}
 	baseFormatter.FullTimestamp = true
@@ -48,10 +56,17 @@ func NewLog(source string) Logger {
 	l.Out = os.Stdout
 	l.Level = logrus.DebugLevel
 
+	fields := logrus.Fields{}
+
 	if source != "" {
-		return (*logger)(l.WithField(sourceKey, source))
+		fields[SourceKey] = source
 	}
-	return (*logger)(l.WithFields(logrus.Fields{}))
+
+	if hostname, err := os.Hostname(); err == nil {
+		fields[HostKey] = hostname
+	}
+
+	return (*logger)(l.WithFields(fields))
 }
 
 // WithField logs the provided key and value as a structured log entry, and returns the entry for further use.
@@ -84,10 +99,11 @@ func (l *logger) Info(args ...interface{}) {
 	(*logrus.Entry)(l).Info(args)
 }
 
-// Info logs at the "warn" level -- this level is suitable for production; the log should be *actionable information*
-// that will be read by a human, or by a machine.
+// Audit logs critical auditing-related information -- this level is suitable for production; the log should be
+// *actionable information* that will be read by a human, or by a machine.
+// Log entries made with Audit will be tagged with the audit field (AuditKey: true).
 func (l *logger) Audit(args ...interface{}) {
-	(*logrus.Entry)(l).WithField(auditKey, true).Warn(args)
+	(*logrus.Entry)(l).WithField(AuditKey, true).Warn(args)
 }
 
 // SetEnableDebug configures whether the "debug" log level is enabled (the default is true).
@@ -105,6 +121,6 @@ func (l *logger) SetEnableDebug(enable bool) {
 // The domain is set via WithField() with a well-defined key.
 // The new Logger will take on all attributes of the current Logger.
 func (l *logger) NewDomainLogger(domain string) *logger {
-	e := (*logrus.Entry)(l).WithField(domainKey, domain)
+	e := (*logrus.Entry)(l).WithField(DomainKey, domain)
 	return (*logger)(e)
 }
