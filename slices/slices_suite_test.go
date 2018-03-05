c16ed0182694fc17ce6ce161b417e5c77e1f6b7a
Mike Post <mike@bitmancer.me>
2018-03-04 20:00:54 -0800

    Move slices stuff from ./util to ./slices in prep for 'go generate' stuff

---
+++ slices/slices_suite_test.go
@@ -0,0 +1,13 @@
+package util_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestSlices(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Slices Suite")
+}
